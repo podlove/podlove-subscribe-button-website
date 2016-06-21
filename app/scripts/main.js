c25c23c05382b247ed7705fd92f1b08b69020b58
Michaela Lehr <michaela.lehr@geildanke.com>
2016-06-21 21:22:41 +0200

    feat (hero): Add hero background image.
    - Write Podlove Subscribe Button uppercase.
    - Add hero background image.
    - Add podcast data to the main script file.
    
    FIXES: Button configurator not working on GitHub pages.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,7 +3,41 @@
 
 $(document).ready(function () {
 
-  var $button = $('#live-button'),
+    window.podcastData = {
+      'title': 'Newz of the World',
+      'subtitle': 'Tim and Mark talk about the Newz™',
+      'description': 'Newz of the World is a weekly show about world news. Mark Fonseca Rendeiro and Tim Pritlove come together to present interesting reports and discuss their aspects and possible consequences. Newz of the world wants to be an alternative window to the common media flow and cherry picks interesting developments for you.',
+      'cover': 'http://meta.metaebene.me/media/newz/newz-logo-600x600.jpg',
+      'feeds': [
+        {
+          'type': 'audio',
+          'format': 'mp3',
+          'url': 'http://newz-of-the-world.com/feed/mp3',
+          'variant': 'high'
+        },
+        {
+          'type': 'audio',
+          'format': 'aac',
+          'url': 'http://newz-of-the-world.com/feed/mp4',
+          'variant': 'high',
+          'directory-url-itunes': 'https://itunes.apple.com/de/podcast/newz-of-the-world/id492588543'
+        },
+        {
+          'type': 'audio',
+          'format': 'ogg',
+          'url': 'http://newz-of-the-world.com/feed/ogg',
+          'variant': 'high'
+        },
+        {
+          'type': 'audio',
+          'format': 'opus',
+          'url': 'http://newz-of-the-world.com/feed/opus',
+          'variant': 'high'
+        }
+      ]
+    };
+
+    var $button = $('#live-button'),
     buttonConfig = {
       'data-json-data': 'podcastData',
       'data-language': 'de',
