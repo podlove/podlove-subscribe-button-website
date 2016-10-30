538c8b3b5c50e9bcd3658daba70b6c7ddec49b09
Michaela Lehr <michaela.lehr@geildanke.com>
2016-10-30 21:48:48 +0100

    fix (gulp): do not lint vendor files

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -52,7 +52,7 @@ const testLintOptions = {
   }
 };
 
-gulp.task('lint', lint('app/scripts/**/*.js'));
+gulp.task('lint', lint('app/scripts/*.js'));
 gulp.task('lint:test', lint('test/spec/**/*.js', testLintOptions));
 
 gulp.task('html', ['styles', 'scripts'], () => {
